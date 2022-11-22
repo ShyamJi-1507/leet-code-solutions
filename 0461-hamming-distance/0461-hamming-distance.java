bc217eb4f7ced892831b413e3fdfd3727299b363
ShyamJi-1507 <113824513+ShyamJi-1507@users.noreply.github.com>
2022-11-22 15:31:48 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (83.58%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int count = 0;
+        int ans = x^y;
+        int mask = 1;
+        for(int i = 0; i < 32; i++){
+            if((ans & mask)!=0){
+                count++;
+                
+            }
+            mask<<=1;
+                
+        }
+        return count;
+        
+    }
+}
